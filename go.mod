79f5c5a7907d353ef3771d3da7319789a7ac7b85
Fredrik Pettersson <fredrik.pettersson.89@gmail.com>
2025-02-03 22:13:57 +0100

    Add tests for error handler

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/labstack/echo-contrib v0.17.2
 	github.com/labstack/echo/v4 v4.13.3
 	github.com/rs/zerolog v1.33.0
+	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.69.2
 	google.golang.org/protobuf v1.36.1
 )
@@ -19,14 +20,17 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.20.5 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.61.0 // indirect
@@ -41,4 +45,5 @@ require (
 	golang.org/x/time v0.8.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
 	gopkg.in/go-jose/go-jose.v2 v2.6.3 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ handler/error-handler_test.go
@@ -0,0 +1,109 @@
+package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/hyperremix/song-contest-rater-service/mapper"
+	"github.com/jackc/pgx/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+type errorResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func TestErrorHandler(t *testing.T) {
+	tests := []struct {
+		name              string
+		err               error
+		responseCommitted bool
+		expectedCode      int
+		expectedMsg       string
+	}{
+		{
+			name:              "Already committed response",
+			err:               errors.New("some error"),
+			responseCommitted: true,
+			expectedCode:      http.StatusOK,
+			expectedMsg:       "",
+		},
+		{
+			name:              "HTTP error",
+			err:               echo.NewHTTPError(http.StatusBadRequest, "bad request"),
+			responseCommitted: false,
+			expectedCode:      http.StatusBadRequest,
+			expectedMsg:       "code=400, message=bad request",
+		},
+		{
+			name:              "Request binding error",
+			err:               mapper.NewRequestBindingError(errors.New("binding failed")),
+			responseCommitted: false,
+			expectedCode:      http.StatusBadRequest,
+			expectedMsg:       "could not bind request",
+		},
+		{
+			name:              "Response binding error",
+			err:               mapper.NewResponseBindingError(errors.New("binding failed")),
+			responseCommitted: false,
+			expectedCode:      http.StatusBadRequest,
+			expectedMsg:       "could not bind response",
+		},
+		{
+			name:              "No rows error",
+			err:               pgx.ErrNoRows,
+			responseCommitted: false,
+			expectedCode:      http.StatusNotFound,
+			expectedMsg:       "no rows in result set",
+		},
+		{
+			name:              "Transaction closed error",
+			err:               pgx.ErrTxClosed,
+			responseCommitted: false,
+			expectedCode:      http.StatusServiceUnavailable,
+			expectedMsg:       "tx is closed",
+		},
+		{
+			name:              "Unknown error",
+			err:               errors.New("unknown error"),
+			responseCommitted: false,
+			expectedCode:      http.StatusInternalServerError,
+			expectedMsg:       "unknown error",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+
+			if tt.responseCommitted {
+				c.NoContent(http.StatusOK)
+			}
+
+			ErrorHandler(tt.err, c)
+
+			if tt.responseCommitted {
+				assert.Equal(t, tt.expectedCode, rec.Code)
+				assert.Empty(t, rec.Body.String())
+				return
+			}
+
+			assert.Equal(t, tt.expectedCode, rec.Code)
+
+			var response errorResponse
+			err := json.Unmarshal(rec.Body.Bytes(), &response)
+			assert.NoError(t, err)
+			assert.Equal(t, strconv.Itoa(tt.expectedCode), response.Code)
+			assert.Equal(t, tt.expectedMsg, response.Message)
+		})
+	}
+}
